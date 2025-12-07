abae473c0d7620cdf5e105af0949ff7cb6cf3c05
Abhinav Prakash <abhinavprakash7352@gmail.com>
2025-12-08 00:06:45 +0530

    Add files via upload

---
+++ Revision for test c/Different_char_pattern/5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int rows;
+    printf("Enter the rows : ");
+    scanf("%d", &rows);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int s = 0; s < rows - i - 1; s++)
+            printf(" ");
+        for (int j = 0; j < 2 * i + 1; j++)
+            printf("%c", 'A' + j);
+        printf("\n");
+    }
+
+    return 0;
+}
